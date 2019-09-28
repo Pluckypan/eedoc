c78d99a34e2866d7a6b24312680668da123782a7
plucky <py@meitu.com>
2019-09-28 23:18:28 +0800

    1.6.5

--- template/nav/js/script.js
+++ template/nav/js/script.js
@@ -161,12 +161,15 @@ $('.entry-content').find('a').each(function() {
 });
 
 function getQueryString(name) {
-	var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-	var r = window.location.search.substr(1).match(reg);
-	if (r != null) {
-		return unescape(r[2]);
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return pair[1];
+		}
 	}
-	return null;
+	return (false);
 }
 
 // 文章目录锚链接滑动
@@ -188,7 +191,9 @@ $(document).ready(function() {
 
 	$(".visibleTag").each(function() {
 		const tag = $(this).attr("visibleTag");
-		if (getQueryString(tag)) {
+		const tagVal = getQueryString(tag);
+		console.log("tag=" + tag + " tagVal=" + tagVal);
+		if (tagVal) {
 			$(this).css("display", "");
 		} else {
 			$(this).css("display", "none");
